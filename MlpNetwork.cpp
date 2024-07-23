e94bc8d1b94e6b2e41cfcf53e4f0222a59fd1bfe
Owner-Hadas <hadas.elezra@mail.huji.ac.il>
2024-07-23 10:05:03 +0300

    WIN !!

--- MlpNetwork.cpp
+++ MlpNetwork.cpp
@@ -1,10 +1,21 @@
 #include "MlpNetwork.h"
 #define SHIFT 1
+#define SUBSCRIPT_OUT_OF_BOUNDS "Matrix subscript out of bounds."
 
 MlpNetwork::MlpNetwork (const Matrix *weights, const Matrix *biases)
 {
   for (int i=0; i< MLP_SIZE; i++)
   {
+    if (weights[i].get_rows() != weights_dims[i].rows || weights[i]
+    .get_cols() != weights_dims[i].cols)
+    {
+      throw std::out_of_range(SUBSCRIPT_OUT_OF_BOUNDS);
+    }
+    if (biases[i].get_rows() != bias_dims[i].rows || biases[i].get_cols()
+          !=bias_dims[i].cols)
+    {
+      throw std::out_of_range(SUBSCRIPT_OUT_OF_BOUNDS);
+    }
     _weights[i]=weights[i];
     _biases[i]=biases[i];
   }
